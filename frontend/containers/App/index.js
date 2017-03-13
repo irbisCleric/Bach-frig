13ea81b9ea861acbcf6a376762ed536929d80cd8
y.budachov <yevhen@backbase.com>
2017-03-13 16:37:45 +0100

    refactor components to dumb component

--- frontend/containers/App/index.js
+++ frontend/containers/App/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+// import React, { Component } from "react";
 // import { bindActionCreators } from "redux";
 // import { connect } from "react-redux";
 
@@ -10,72 +11,61 @@ import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowCol
 
 import style from "./style.css";
 
-class Frig extends Component {
-    render() {
-        const foodsList = [
-            { title: "Meat", amount: 2 },
-            { title: "Chicken", amount: 3 },
-            { title: "Tomato", amount: 1 },
-        ];
+function Frig() {
+    const foodsList = [
+        { title: "Meat", amount: 2 },
+        { title: "Chicken", amount: 3 },
+        { title: "Tomato", amount: 1 },
+    ];
 
-        const tBody = foodsList.map(food => (
-          <TableRow key={food.title}>
-            <TableRowColumn>{food.title}</TableRowColumn>
-            <TableRowColumn>{food.amount}</TableRowColumn>
+    const tBody = foodsList.map(food => (
+      <TableRow key={food.title}>
+        <TableRowColumn>{food.title}</TableRowColumn>
+        <TableRowColumn>{food.amount}</TableRowColumn>
+      </TableRow>
+      ),
+    );
+
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>Food</TableHeaderColumn>
+            <TableHeaderColumn>Amount</TableHeaderColumn>
           </TableRow>
-          ),
-        );
-
-        const FoodInFrig = () => (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHeaderColumn>Food</TableHeaderColumn>
-                <TableHeaderColumn>Amount</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              { tBody }
-            </TableBody>
-          </Table>
-        );
-
-        return (
-          <FoodInFrig />
-        );
-    }
+        </TableHeader>
+        <TableBody>
+          { tBody }
+        </TableBody>
+      </Table>);
 }
 
-class CheckFrig extends Component {
-    render() {
-        const btnText = "Check frig";
-        const btnIsPrimary = true;
+function CheckFrig() {
+    const btnText = "Check frig";
+    const btnIsPrimary = true;
 
-        return (
-          <RaisedButton
-            label={btnText}
-            primary={btnIsPrimary}
-            icon={<ActionAndroid />}
-          />
-        );
-    }
+    return (
+      <RaisedButton
+        label={btnText}
+        primary={btnIsPrimary}
+        icon={<ActionAndroid />}
+      />
+    );
 }
 
-class App extends Component {
-    render() {
-        return (
-          <MuiThemeProvider>
-            <div className={style.MainContainer}>
-              <div className={style.RightColumn}>
-                <CheckFrig />
-              </div>
-              <div className={style.LeftColumn}>
-                <Frig />
-              </div>
-            </div>
-          </MuiThemeProvider>
-        );
-    }
+function App() {
+    return (
+      <MuiThemeProvider>
+        <div className={style.MainContainer}>
+          <div className={style.RightColumn}>
+            <CheckFrig />
+          </div>
+          <div className={style.LeftColumn}>
+            <Frig />
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
 }
 
 export default App;
