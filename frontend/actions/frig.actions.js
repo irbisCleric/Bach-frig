0f8792901845e4f8c40253d3114219220badc520
y.budachov <yevhen@backbase.com>
2017-03-15 10:12:31 +0100

    rename variable

--- frontend/actions/frig.actions.js
+++ frontend/actions/frig.actions.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import constants from "./../constants/action.types";
 import appConstants from "./../constants/app.constants";
 import apiActions from "./api.actions";
@@ -6,19 +5,13 @@ import apiActions from "./api.actions";
 import Api from "../api/ApiClient";
 import ep from "../constants/endPoints.constant";
 
-const test = new Api({ baseURL: appConstants.API_URL });
+// TODO maybe move to separate file
+const apiFactory = new Api({ baseURL: appConstants.API_URL });
 
 export const getFrigItems = () => (dispatch) => {
-    test.get(ep.frig.getFrigItems())
+    apiFactory.get(ep.frig.getFrigItems())
         .then((response) => {
             dispatch(apiActions.success(constants.FETCH_FRIG_ITEMS, response));
-        });
-};
-
-export const addFrigItem = () => (dispatch) => {
-    axios.get(constants.API_URL)
-        .then((response) => {
-            dispatch(apiActions.success(appConstants.FETCH_FRIG_ITEMS, response));
         });
 };
 
