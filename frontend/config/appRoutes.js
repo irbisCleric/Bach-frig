ddc7a1e31143570b6e0414cf07950a1d055fb08e
Serge <someemail@in.ua>
2017-04-10 15:58:27 +0100

    404 routing. Removed excess file.

--- frontend/config/appRoutes.js
+++ frontend/config/appRoutes.js
@@ -3,17 +3,18 @@ import { Route, IndexRoute, IndexRedirect } from "react-router";
 
 import App from "./../containers/App";
 import FormAddingMeal from "./../containers/FormAddingMeal";
-import Layout from "./../containers/layouts/Layout.container";
+// import Layout from "./../containers/layouts/Layout.container";
+
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>);
 
 export default (
   <Route path="/" component={App}>
-    <Route component={Layout}>
-      <IndexRedirect to="app" />
-
-      <Route path="app">
-        <IndexRoute component={App} />
-      </Route>
+    <IndexRedirect to="app" />
+    <Route path="app">
+      <IndexRoute component={App} />
     </Route>
-    <Route path="/add_meal" component={FormAddingMeal} />
+    <Route path="add_meal" component={FormAddingMeal} />
+    <Route path="*" component={NotFound} />
   </Route>
 );
